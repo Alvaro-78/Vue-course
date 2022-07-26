d79b1efc2039603f092ca43cc7b265f263687322
alvaro-78 <alvaro78an@gmail.com>
2022-07-26 19:58:41 +0200

    Added formCrud

---
+++ formCrud/src/components/Input.vue
@@ -0,0 +1,66 @@
+<template lang="">
+	<input
+		type="text"
+		class="form-control mt-2"
+		placeholder="Nombre de la tarea"
+		v-model.trim="task.name"
+	/>
+	<div class="form-check form-check-inline">
+		<input
+			class="form-check-input"
+			type="checkbox"
+			id="Checkbox1"
+			value="Javascript"
+			v-model="task.categories"
+		/>
+		<label class="form-check-label" id="Checkbox1">Javascript</label>
+	</div>
+	<div class="form-check form-check-inline">
+		<input
+			class="form-check-input"
+			type="checkbox"
+			id="Checkbox2"
+			value="Node.js"
+			v-model="task.categories"
+		/>
+		<label class="form-check-label" id="Checkbox2"> Node.js </label>
+	</div>
+	<div class="form-check form-check-inline">
+		<input
+			class="form-check-input"
+			type="radio"
+			name="inlineRadioOptions"
+			id="Radio1"
+			value="urgente"
+			v-model="task.state"
+		/>
+		<label class="form-check-label" for="Radio1">Urgente</label>
+	</div>
+	<div class="form-check form-check-inline">
+		<input
+			class="form-check-input"
+			type="radio"
+			name="inlineRadioOptions"
+			id="Radio2"
+			value="relax"
+			v-model="task.state"
+		/>
+		<label class="form-check-label" for="Radio2">Relax</label>
+	</div>
+	<div class="mt-2">
+		<input type="number" class="form-control" v-model.trim="task.number" />
+	</div>
+	<button class="btn btn-dark mt-2 btn-block" :disabled="block">Guardar</button>
+</template>
+<script>
+	export default {
+		props: {
+			task: Object,
+		},
+		computed: {
+			block() {
+				return this.task.name.trim() === '' ? true : false;
+			},
+		},
+	};
+</script>
--- formCrud/src/store/index.js
+++ formCrud/src/store/index.js
@@ -1,14 +1,53 @@
-import { createStore } from 'vuex'
+import { createStore } from 'vuex';
+import router from '../router';
 
 export default createStore({
-  state: {
-  },
-  getters: {
-  },
-  mutations: {
-  },
-  actions: {
-  },
-  modules: {
-  }
-})
+	state: {
+		tasks: [],
+		task: {
+			id: '',
+			name: '',
+			categories: [],
+			state: '',
+			number: 0,
+		},
+	},
+	getters: {},
+	mutations: {
+		set(state, payload) {
+			state.tasks.push(payload);
+			console.log(state.tasks);
+		},
+		delete(state, payload) {
+			state.tasks = state.tasks.filter((item) => item.id !== payload);
+		},
+		task(state, payload) {
+			state.task = state.tasks.find((item) => item.id === payload);
+		},
+		update(state, payload) {
+			console.log(payload);
+			const payloadId = payload.id;
+			state.tasks = state.tasks.map((item) => item.id === payloadId)
+				? payload
+				: item;
+			router.push('/');
+			return;
+		},
+	},
+	actions: {
+		setTasks({ commit }, task) {
+			commit('set', task);
+		},
+		deleteTask({ commit }, id) {
+			commit('delete', id);
+		},
+		setTask({ commit }, id) {
+			commit('task', id);
+		},
+		updateTask({ commit }, task) {
+			console.log('click en update');
+			commit('update', task);
+		},
+	},
+	modules: {},
+});
